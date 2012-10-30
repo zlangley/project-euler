6620707022181c5ac71f180b1362f6038dd353e8
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 17:11:50 -0400

    adding solution to problem 15

---
+++ 015.rb
@@ -0,0 +1,7 @@
+grid = Array.new(21) { Array.new(21, 1) }
+1.upto(grid.size - 1) do |r|
+  1.upto(grid[r].size - 1) do |c|
+    grid[r][c] = grid[r][c - 1] + grid[r - 1][c]
+  end
+end
+puts grid[20][20]
