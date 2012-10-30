4b5e8c6324c3749ad1a4892ce715a5111c439737
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 16:50:21 -0400

    not using mathn library for problem 7

--- 007.rb
+++ 007.rb
@@ -1,2 +1,12 @@
-require 'mathn'
-puts Prime.take(10001).last
+def primes_less_than(n)
+  s = (0..n).to_a
+  s[0] = s[1] = nil
+  s.each do |p|
+    next unless p
+    break if p * p > n
+    (p*p).step(n, p) { |m| s[m] = nil }
+  end
+  s.compact
+end
+# pi(n) > n / ln(n), so 10001st prime is less than 116684
+puts primes_less_than(116684).at(10000)
