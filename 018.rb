27b9194fab41e2ced23f48b84da104e65e4c4001
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 17:49:51 -0400

    adding solution to problem 18

---
+++ 018.rb
@@ -0,0 +1,31 @@
+triangle = <<END.split("\n").map { |line| line.split.map(&:to_i) }
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+END
+
+table = Array.new(triangle.size) { Array.new(triangle.last.size, 0) }
+table[0][0] = triangle[0][0]
+1.upto(table.size - 1) do |r|
+  triangle[r].size.times do |c|
+    table[r][c] = triangle[r][c]
+    if c > 0
+      table[r][c] += [table[r-1][c], table[r-1][c-1]].max 
+    else
+      table[r][c] += table[r-1][c]
+    end
+  end
+end
+puts table.last.max
