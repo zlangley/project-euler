767c947a25a0139e7f0fb2f6fde3e967f33a25d7
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 16:51:43 -0400

    exiting when found in problem 9

--- 009.rb
+++ 009.rb
@@ -1,6 +1,9 @@
-(1..1000).flat_map do |a|
-	ans = (a..1000).map do |b|
-		c = 1000 - a - b
-		puts a*b*c if a*a + b*b == c*c
-	end
+(1..1000).map do |a|
+  ans = (a..1000).map do |b|
+    c = 1000 - a - b
+    if a*a + b*b == c*c
+      puts a*b*c
+      exit
+    end
+  end
 end
