c1287124ecaa17164c917856c17f6391575c7b19
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 16:52:29 -0400

    not using mathn library for problem 10

--- 010.rb
+++ 010.rb
@@ -1,2 +1,13 @@
-require 'mathn'
-puts Prime.take_while { |n| n < 2_000_000 }.inject(:+)
+sum = 0
+is_prime = Array.new(2_000_000, true)
+
+i = 2
+while i*i < 2_000_000
+  if is_prime[i]
+    sum += i
+    (i+i..2_000_000).step(i) { |j| is_prime[j] = false }
+  end
+  i += 1
+end
+i.upto(2_000_000) { |j| sum += j if is_prime[j] }
+puts sum
