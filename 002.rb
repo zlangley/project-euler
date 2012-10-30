3de97cfc971879193fd027e1841993a2c06412a9
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 16:43:55 -0400

    rewriting problem 2

--- 002.rb
+++ 002.rb
@@ -1,9 +1,6 @@
-Fibonacci = Enumerator.new do |yielder|
-	a, b = 1, 1
-	loop do
-		yielder.yield a
-		a, b = b, a + b
-	end
+sum, a, b = 0, 1, 2
+while b < 4_000_000
+  sum += b if b.even?
+  a, b = b, a + b
 end
-
-puts Fibonacci.take_while { |f| f < 4e6 }.select(&:even?).inject(:+)
+puts sum
