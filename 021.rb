5af7c2221bcbf9ed3df7f3c869c09cbc353bff4a
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 22:12:57 -0400

    cleaning up problem 21

--- 021.rb
+++ 021.rb
@@ -1,21 +1,21 @@
-class Integer
-  def divisors
-    d = []
-    1.upto(Math.sqrt(self).round) do |i|
-      if self % i == 0
-        d << i
-        d << self / i if self / i != i
-      end
+def divisor_sum(n)
+  sum, i = 1, 2
+  while i*i <= n
+    if n % i == 0
+      sum += i
+      sum += n/i if i != n/i
     end
-    d
+    i += 1
   end
+  sum
 end
 
-sum = 0
-1.upto(10000) do |n|
-  m = n.divisors.inject(:+) - n
-  if n < m && m.divisors.inject(:+) - m == n
-    sum += n + m
+sum = (1..10000).inject do |a,n|
+  m = divisor_sum(n)
+  if n < m && divisor_sum(m) == n
+    a + n + m
+  else
+    a
   end
 end
 puts sum
