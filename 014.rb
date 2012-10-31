1be051c17b2a1bd04f952dfc313b95dda8322381
Zach Langley <zbl9222@cs.rit.edu>
2012-10-30 21:50:51 -0400

    using < instead of <=

--- 014.rb
+++ 014.rb
@@ -6,7 +6,7 @@ def chain_length(n)
     @cache[n]
   else
     ans = 1 + (n.even? ? chain_length(n/2) : chain_length(3*n + 1))
-    @cache[n] = ans if n <= @cache.size
+    @cache[n] = ans if n < @cache.size
     ans
   end
 end
