06d72f6a22b03e82db8ba60d84b0a37eccd0dd7e
Zach Langley <zbl9222@cs.rit.edu>
2012-11-01 20:59:04 -0400

    speeding up problem 34

--- 034.rb
+++ 034.rb
@@ -1,3 +1,10 @@
 fact = [1]
 1.upto(9) { |i| fact[i] = i * fact[i-1] }
-puts (3..1000000).select { |n| n.to_s.chars.map { |c| fact[c.to_i] }.inject(:+) == n }.inject(:+)
+puts (3..1000000).select { |n| 
+  copy, sum = n, 0
+  while copy > 0
+    sum += fact[copy % 10]
+    copy /= 10
+  end
+  sum == n
+}.inject(:+)
