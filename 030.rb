436d8463246de7162469ae46a3dc788981f1f5b1
Zach Langley <zbl9222@cs.rit.edu>
2012-11-01 21:00:25 -0400

    speeding up problem 30

--- 030.rb
+++ 030.rb
@@ -1,1 +1,8 @@
-puts (2..350000).select { |n| n.to_s.chars.map { |c| c.to_i**5 }.inject(:+) == n }.inject(:+)
+puts (2..350000).select { |n| 
+  copy, sum = n, 0
+  while copy > 0
+    sum += (copy % 10)**5
+    copy /= 10
+  end
+  sum == n
+}.inject(:+)
