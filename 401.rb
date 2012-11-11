4e4c0ca70d026921972e5395527c1872a68e89cb
Zach Langley <zbl9222@cs.rit.edu>
2012-11-11 13:06:00 -0500

    cleaning up solution for problem 401

--- 401.rb
+++ 401.rb
@@ -1,38 +1,15 @@
-MOD = 1000000000 
 def sum_of_squares(n)
   n*(2*n + 1)*(n + 1) / 6
 end
 
-def next_square(n)
-  i = 1
+def sigma(n)
+  res, i = 0, 1
   while i*i <= n
+    res += (n / i) * i * i
+    res += i * (sum_of_squares(n/i) - sum_of_squares(n/(i+1))) if n/i != i
     i += 1
   end
-  i * i
+  res
 end
 
-def sigma(n)
-  if n == 1
-    1
-  else
-    res = 2 * sigma(n / 2)
-    i = 1
-    nsq = next_square(n)
-    while i*i <= nsq
-      other = n / i
-      if other % 2 == 1 && i != nsq / i && i != other
-        res += i*i
-        res %= MOD
-      end
-      if i % 2 == 0
-        last = n / (i - 1)
-        res += sum_of_squares(last) - sum_of_squares(other)
-        res %= MOD
-      end
-      i += 1
-    end
-    res
-  end
-end
-
-puts sigma(1000000000000000)
+puts sigma(1_000_000_000_000_000) % 1_000_000_000
